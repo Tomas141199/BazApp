48182caaf1760dd7634fc15f1dad9a2d8523672b
Tomas141199 <59587765+Tomas141199@users.noreply.github.com>
2021-11-07 15:11:23 -0600

    State categorias

--- components/Categorias.js
+++ components/Categorias.js
@@ -1,31 +1,32 @@
-
-const Categorias = ({ cat , setCategorias }) => {
-    
-  const seleccionar = (e) =>{
-      console.log(e.target.key);
-
-      if (e.target.classList.contains("linea-input")) {
-          e.target.classList.remove("linea-input");
-          e.target.classList.add("bg-bazapp","text-white");
-          
-          
-      } else {
-        e.target.classList.add("linea-input");
-        e.target.classList.remove("bg-bazapp","text-white");  
-      }
-
-  }
-  const categorias = cat;
-  
-  const lista = categorias.map((categoria ) =>
-    <li key={categoria.id} value={categoria.titulo}  name={categoria.titulo} onClick={seleccionar}
-    className="btn m-2 linea-input">{categoria.titulo}</li>
-  );
-
- 
-  return (
-      <ul>{lista}</ul>
-    );
+const Categorias = ({ cat, setCategorias, categories }) => {
+  const seleccionar = (e) => {
+    if (e.target.classList.contains("linea-input")) {
+      e.target.classList.remove("linea-input");
+      e.target.classList.add("bg-bazapp", "text-white");
+      setCategorias([...categories, e.target.textContent]);
+    } else {
+      e.target.classList.add("linea-input");
+      e.target.classList.remove("bg-bazapp", "text-white");
+      setCategorias(
+        categories.filter((categorie) => categorie != e.target.textContent)
+      );
+    }
   };
-  
-  export default Categorias;
+  const categorias = cat;
+
+  const lista = categorias.map((categoria) => (
+    <li
+      key={categoria.id}
+      value={categoria.titulo}
+      name={categoria.titulo}
+      onClick={seleccionar}
+      className="btn m-2 linea-input"
+    >
+      {categoria.titulo}
+    </li>
+  ));
+
+  return <ul>{lista}</ul>;
+};
+
+export default Categorias;
--- pages/publicar.js
+++ pages/publicar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Layout from "../components/layouts/Layout"; 
+import Layout from "../components/layouts/Layout";
 import Categorias from "../components/Categorias";
 import styled from "@emotion/styled";
 import { FireContext } from "../fire";
@@ -7,88 +7,136 @@ import { useContext } from "react";
 import Router from "next/router";
 
 const AgregarProducto = () => {
+  const [fileurl, setFileUrl] = useState(
+    "https://mdbootstrap.com/img/Photos/Others/placeholder.jpg"
+  ); //aqui pocemos poner el que está por feault
 
-    const [fileurl, setFileUrl] = useState("https://mdbootstrap.com/img/Photos/Others/placeholder.jpg"); //aqui pocemos poner el que está por feault
+  const [categorias, setCategorias] = useState([]);
 
-    const [categorias,setCategorias] = useState([]);
+  function prevIMG(input) {
+    const imagenfile = input.target.files[0];
+    const imageUrl = URL.createObjectURL(imagenfile);
+    console.log(imageUrl);
+    setFileUrl(imageUrl);
+  }
 
-    function prevIMG(input){
-        const imagenfile = input.target.files[0];
-        const imageUrl = URL.createObjectURL(imagenfile);
-        console.log(imageUrl);
-        setFileUrl(imageUrl)
-      }
+  //arreglo de categorias
+  const TiposCategorias = [
+    { id: 1, titulo: "Accesorios" },
+    { id: 2, titulo: "Bolsas" },
+    { id: 3, titulo: "Calzado" },
+    { id: 4, titulo: "Chamarras" },
+    { id: 5, titulo: "Pantalones" },
+    { id: 6, titulo: "Sacos y buzos" },
+    { id: 7, titulo: "tops" },
+    { id: 8, titulo: "Vestidos" },
+    { id: 9, titulo: "Caballero" },
+    { id: 10, titulo: "Dama" },
+    { id: 11, titulo: "Infantes" },
+  ];
 
-    //arreglo de categorias
-    const TiposCategorias = [{id:1,titulo:"Accesorios"}
-                            ,{id:2,titulo:"Bolsas"}
-                            ,{id:3,titulo:"Calzado"}
-                            ,{id:4,titulo:"Chamarras"}
-                            ,{id:5,titulo:"Pantalones"}
-                            ,{id:6,titulo:"Sacos y buzos"}
-                            ,{id:7,titulo:"tops"}
-                            ,{id:8,titulo:"Vestidos"}
-                            ,{id:9,titulo:"Caballero"}
-                            ,{id:10,titulo:"Dama"}
-                            ,{id:11,titulo:"Infantes"}];
+  return (
+    <div className="mt-nvar">
+      <Layout />
 
-    return (
-        <div className="mt-nvar">
-        <Layout/>
-        
-        <div className="container w-75 bg-white mt-5 rounded shadow">
+      <div className="container w-75 bg-white mt-5 rounded shadow">
         <div className="row align-items-stretch mb-3 abs-center">
-            <div className="col p-5  rounded-end">
-            <h3 className="my-3 text-center titulo_morado">¿Qué quieres vender?</h3>
+          <div className="col p-5  rounded-end">
+            <h3 className="my-3 text-center titulo_morado">
+              ¿Qué quieres vender?
+            </h3>
             <form>
-                <div className="form-group">
-                    <label htmlFor="nombre">Nombre</label>
-                    <input type="text" className="form-control" id="nombre" name="nombre" aria-describedby="emailHelp" placeholder="Producto"/>
+              <div className="form-group">
+                <label htmlFor="nombre">Nombre</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="nombre"
+                  name="nombre"
+                  aria-describedby="emailHelp"
+                  placeholder="Producto"
+                />
+              </div>
+              <div className="row mt-3 ">
+                <div className="col-md-6">
+                  <label htmlFor="precio" className="form-label">
+                    Precio
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="precio"
+                    name="precio"
+                    placeholder="$0.000"
+                  />
                 </div>
-                <div className="row mt-3 ">
-                    <div className="col-md-6">
-                        <label htmlFor="precio" className="form-label">Precio</label>
-                        <input type="text" className="form-control" id="precio" name="precio" placeholder="$0.000"/>
-                    </div>
-                    <div className="col-md-6">
-                        <label htmlFor="talla" className="form-label">Talla</label>
-                        <input type="text" className="form-control" id="talla" placeholder="CH"/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="descripcion" className="form-label">Descripción</label>
-                        <textarea className="form-control" id="descripcion" name="descripcion" rows="3"></textarea>
-                    </div>
+                <div className="col-md-6">
+                  <label htmlFor="talla" className="form-label">
+                    Talla
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="talla"
+                    placeholder="CH"
+                  />
                 </div>
-                <p>Categorias</p>
+                <div className="mb-3">
+                  <label htmlFor="descripcion" className="form-label">
+                    Descripción
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="descripcion"
+                    name="descripcion"
+                    rows="3"
+                  ></textarea>
+                </div>
+              </div>
+              <p>Categorias</p>
 
-                <Categorias cat={TiposCategorias} setCategorias={setCategorias}/>
-                
-                <div className="text-center">
-                    <button type="button" className="mt-3 btn bg-bazapp text-white"> <img src="/mas.png" className="bg-mas"/> Guardar</button>
-                </div>
-                
+              <Categorias
+                cat={TiposCategorias}
+                setCategorias={setCategorias}
+                categories={categorias}
+              />
+
+              <div className="text-center">
+                <button type="button" className="mt-3 btn bg-bazapp text-white">
+                  <span className="fs-5 font-weight-bold">&#43;</span>
+                  Guardar
+                </button>
+              </div>
             </form>
-            </div>
-            <div className="col align-items-stretch  abs-center">
-                <div className="file-field">
-                    <div className="z-depth-1-half mb-4">
-                    <img src={fileurl} id="previsualizar" className="img-fluid"
-                        alt="example placeholder"/>
-                    </div>
-                    <div className="d-flex justify-content-center">
-                    <div className="btn btn-mdb-color btn-rounded float-left">
-                        <p>Eligen una foto del producto</p>
-                        <input type="file" className='form-control' name="imagen" accept="image/*" onChange={prevIMG}/>
-                    </div>
-                    </div>
+          </div>
+          <div className="col align-items-stretch  abs-center">
+            <div className="file-field">
+              <div className="z-depth-1-half mb-4">
+                <img
+                  src={fileurl}
+                  id="previsualizar"
+                  className="img-fluid"
+                  alt="example placeholder"
+                />
+              </div>
+              <div className="d-flex justify-content-center">
+                <div className="btn btn-mdb-color btn-rounded float-left">
+                  <p>Eligen una foto del producto</p>
+                  <input
+                    type="file"
+                    className="form-control"
+                    name="imagen"
+                    accept="image/*"
+                    onChange={prevIMG}
+                  />
                 </div>
+              </div>
             </div>
-            
-
+          </div>
         </div>
       </div>
-      </div>
-     );
-}
- 
+    </div>
+  );
+};
+
 export default AgregarProducto;
