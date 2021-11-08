aa3ac1d6dc509fae99f9115c69efba1f533911ff
Tomas141199 <59587765+Tomas141199@users.noreply.github.com>
2021-11-07 22:35:55 -0600

    Publicar articulo

--- pages/publicar.js
+++ pages/publicar.js
@@ -89,42 +89,46 @@ const AgregarProducto = () => {
     //Se sube el archivo y el metadata con el nombre de imagen file junto con el nombre de la caperta donde se van almacenar(articulos)
     //Esta carpeta si es que no existe storage la crea y almacena el archivo ahi, no es necesario crearla previamente
     //Ref almacena la referencia (direccion) donde se encuntra la imagen
-    const storageRef = ref(storage, "publicaciones/" + imagenfile.name);
-    //uploadTask permite dar seguimiento a la subida de la imagen
-    const uploadTask = uploadBytesResumable(storageRef, imagenfile, metadata);
+    try {
+      const storageRef = ref(storage, "publicaciones/" + imagenfile.name);
+      //uploadTask permite dar seguimiento a la subida de la imagen
+      const uploadTask = uploadBytesResumable(storageRef, imagenfile, metadata);
 
-    //Seguimiento de los estados de cambio , errores y exito de la subida de la imagen
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        //Obtiene el progreso de la tarea incluido el numero de bytes subidos y el total de byte que van a ser almacenados
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
+      //Seguimiento de los estados de cambio , errores y exito de la subida de la imagen
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          //Obtiene el progreso de la tarea incluido el numero de bytes subidos y el total de byte que van a ser almacenados
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          //Si es que hay un error lo muestra por consola, lo ideal seria obtener el set de error para mostrarselo al usuario
+          console.log(error);
+        },
+        () => {
+          // Si es que se subio correctamente la imagen este metodo obtiene la URL de donde se encuentra almcenada la imagen en Storage
+          //Igual checar en firebase
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+            //Se guarda la url en el state, solo funcionaria aqui ya que es un metodo asyncrono y funciones fuera de esta cadena de promesas
+            //no obtendria el valor
+            setFileUrl(downloadURL);
+          });
         }
-      },
-      (error) => {
-        //Si es que hay un error lo muestra por consola, lo ideal seria obtener el set de error para mostrarselo al usuario
-        console.log(error);
-      },
-      () => {
-        // Si es que se subio correctamente la imagen este metodo obtiene la URL de donde se encuentra almcenada la imagen en Storage
-        //Igual checar en firebase
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log("File available at", downloadURL);
-          //Se guarda la url en el state, solo funcionaria aqui ya que es un metodo asyncrono y funciones fuera de esta cadena de promesas
-          //no obtendria el valor
-          setFileUrl(downloadURL);
-        });
-      }
-    );
+      );
+    } catch (error) {
+      setError("Error al cargar la imagen intente de nuevo");
+    }
   }
 
   return (
@@ -207,7 +211,11 @@ const AgregarProducto = () => {
                 setCategorias={setCategorias}
                 categories={categorias}
               />
-              {categorias==""? <Error message="Selecciona por lo menos una categoría"/> : null}
+              {categorias == "" ? (
+                <p className="alert alert-warning text-center p-1">
+                  Selecciona almenos una categoria
+                </p>
+              ) : null}
 
               <input type="hidden" />
               <div className="text-center">
@@ -241,6 +249,7 @@ const AgregarProducto = () => {
                     onInput={prevIMG}
                   />
                   {errores.foto && <Error message={errores.foto} />}
+                  {error && <Error message={error} />}
                 </div>
               </div>
             </div>
