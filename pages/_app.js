959b27a5afcd5de4655cec456d000e503d5a880a
Tomas141199 <59587765+Tomas141199@users.noreply.github.com>
2021-11-14 23:33:08 -0600

    Progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,20 @@ import fire, { FireContext } from "../fire";
 import useAuth from "../hooks/useAuth";
 import { useEffect } from "react";
 import "../styles/globals.css";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+//Config Progress bar
+const progress = new ProgressBar({
+  size: 4,
+  color: "#915480",
+  className: "bar-of-progress",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
   const usuario = useAuth();
